5274c2bbd8e84b1f0a10fd7ae0054286085ae7e1
Rowena <rluk@dimagi.com>
2012-01-14 11:24:47 -0500

    add a setting to specify whether the non-logged-in response should be a HttpResponseForbidden message (machine-friendly) or the login page (user-friendly) (cherry picked from commit 3eafa7d56429cdc5f72f092202b1376ee16e85fd)

--- email_reports/decorators.py
+++ email_reports/decorators.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseForbidden
+from django.http import HttpResponseRedirect
 from django.conf import settings
 
 def magic_token_required():
@@ -13,6 +14,14 @@ def magic_token_required():
             if user.is_authenticated() and user.is_active or \
                "magic_token" in request.REQUEST and request.REQUEST["magic_token"] == settings.MAGIC_TOKEN:
                 return f(request, *args, **kwargs)
+            if settings.EMAIL_REPORTS_HUMAN_FRIENDLY_LOGIN_MESSAGE == True:
+                require_login_path = getattr(settings, 'REQUIRE_LOGIN_PATH', '/accounts/login/')
+                if request.POST:
+                    return HttpResponseRedirect(require_login_path)
+                else:
+                    return HttpResponseRedirect('%s?next=%s' % (require_login_path, request.path))
             return HttpResponseForbidden("You have to be logged in or have the magic token to do that!")
         return require_magic_token
     return wrapper
+
+
---
+++ email_reports/settings.py
@@ -0,0 +1,4 @@
+#this determines whether non-logged-in users get a HttpResponseRedirct
+#or a 200 login page when accessing a view labelled MAGIC_TOKEN_REQUIRED
+EMAIL_REPORTS_HUMAN_FRIENDLY_LOGIN_MESSAGE=False
+
