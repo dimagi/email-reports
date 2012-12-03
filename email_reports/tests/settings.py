61e55a4812fb78ab73e14466050053e5b2ca1171
Mark Lavin <mlavin@caktusgroup.com>
2012-12-03 09:46:10 -0500

    Reorganize tests to include test settings to run stand-alone.

---
+++ email_reports/tests/settings.py
@@ -0,0 +1,27 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = (
+    'django_nose',
+    'djtables',
+    'rapidsms',
+    'rapidsms.contrib.locations',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'djcelery',
+    'logistics',
+    'email_reports',
+)
+
+SITE_ID = 1
+        
+SECRET_KEY = 'secert-key-for-testing'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
